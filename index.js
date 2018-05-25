fc474eaa6b2364d375c870a83dc5a37c8abd9d0b
pashangshangpo <pashangshangpo@gmail.com>
2018-05-25 13:19:04 +0800

    优化log方法

--- index.js
+++ index.js
@@ -5,16 +5,16 @@
  */
 
 ;(function (g) {
-  // 上一次log使用的名称
-  let preLogName = '';
+  var prefix = 'pssp-log-';
+  var preLogName = '';
 
   /** 
   * @start-def: Log: name, color
   *  name: String 名称或message
   *  color: String 颜色
   */
-  const Log = function (name, color) {
-    if (this.constructor === Log) {
+  var Log = function (name, color) {
+    if (this && this.constructor === Log) {
       this.name = name;
       this.color = color;
     }
@@ -29,58 +29,67 @@
         console.log('%c' + name, 'color:' + color);
       }
       else {
-        console.log(name)
+        console.log(name);
       }
 
       preLogName = '';
     }
   };
-  
+
   /**
-   * @def: .message: message, color
-   *  message: String log信息
-   *  color: String 颜色
+   * @def: .message: ...messages
+   *  message: * log信息
    *  输出信息
    */
-  Log.prototype.message = function (message, color) {
+  Log.prototype.message = function (...messages) {
+    var message = '';
+
     if (this.__isOutMessage()) {
       if (preLogName !== this.name) {
         console.log('---- ' + this.name + ' ----');
       }
-      
-      if (typeof message === 'string') {
-        console.log('%c' + message, 'color:' + (color || this.color));
+
+      if (messages.length > 1) {
+        console.log.apply(null, messages);
       }
       else {
-        console.log(message)
+        message = messages[0];
+
+        if (typeof message === 'string') {
+          console.log('%c' + message, 'color:' + this.color);
+        }
+        else {
+          console.log(message);
+        }
       }
 
       preLogName = this.name;
     }
   };
-  
+
   /**
    * @def: .hideMessage:
    * 隐藏log
    */
   Log.prototype.hideMessage = function () {
-    localStorage.setItem('log-' + this.name, 'false');
+    localStorage.setItem(prefix + this.name, 'false');
   };
-  
+
   /**
    * @def: .showMessage:
    * 显示log
    */
   Log.prototype.showMessage = function () {
-    localStorage.setItem('log-' + this.name, 'true');
+    localStorage.setItem(prefix + this.name, 'true');
   };
-  
+
   /**
    * @def: .__isOutMessage:
    * 判断是否输出log信息
    */
   Log.prototype.__isOutMessage = function () {
-    const state = localStorage.getItem('log-' + this.name)
+    var state = localStorage.getItem(prefix + this.name);
+    
     return state === 'true' || state == null;
   };
 
